dc13814bee9a61ef0173227a4bc79b84d049f467
Trevor-Clifton <trevor.c048@stu.nebo.edu>
2025-10-02 13:34:52 -0600

    Fixed walk cycle, made idle animation

---
+++ Project/Assets/Move.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Vector3 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += Vector3.forward * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector3.left * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += -Vector3.forward * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector3.right * 5 * Time.deltaTime;
+        }
+        movement.Normalize();
+        transform.Translate(movement* 5 * Time.deltaTime);
+        movement = Vector3.zero;
+    }
+}
